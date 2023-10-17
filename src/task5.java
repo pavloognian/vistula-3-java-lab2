ab6184d7a8c99f7a8f6e3bb25319dfe1f5f91f55
Pavlo_Og <pavel.ognynanyk@gmail.com>
2023-10-17 21:37:33 +0200

    added file task5

---
+++ src/task5.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.valueOf;
+
+public class task5 {
+    public static void main(String[] args) {
+        BigDecimal product_cost = new BigDecimal(9.99);
+        System.out.println("Product cost(net): "+product_cost);
+        BigDecimal product_cost_gross = product_cost.multiply(valueOf(1.23));
+        System.out.println("Product cost(gross): "+product_cost_gross);
+        BigDecimal product_cost_gross_mul = product_cost_gross.multiply(valueOf(10_000));
+        System.out.println("Product cost(gross) after multiplication(10 000): "+product_cost_gross);
+        BigDecimal product_cost_result = product_cost_gross_mul.divide(valueOf(1.23));
+        System.out.println("Product cost(net) after multiplication(10 000): "+product_cost_result);
+    }
+}
--- src/tasks2_3_4.java
+++ src/tasks2_3_4.java
@@ -2,12 +2,11 @@ import java.math.BigDecimal;
 
 import static java.math.BigDecimal.*;
 
-public class tasks {
+public class tasks2_3_4 {
     public static void main(String[] args) {
         //task2();
         //task3();
         //task4();
-        //task5();
     }
     public static void task2(){
         float earthWeightOgnianyk;
@@ -70,17 +69,5 @@ public class tasks {
             amountOfFullContainers = amountOfBricks%containerCapacity;
             System.out.println("Amount of bricks in 'not full container': "+amountOfFullContainers);
         }
-
-
-    }
-    public static  void task5(){
-        BigDecimal product_cost = new BigDecimal(9.99);
-        System.out.println("Product cost(net): "+product_cost);
-        BigDecimal product_cost_gross = product_cost.multiply(valueOf(1.23));
-        System.out.println("Product cost(gross): "+product_cost_gross);
-        BigDecimal product_cost_gross_mul = product_cost_gross.multiply(valueOf(10_000));
-        System.out.println("Product cost(gross) after multiplication(10 000): "+product_cost_gross);
-        BigDecimal product_cost_result = product_cost_gross_mul.divide(valueOf(1.23));
-        System.out.println("Product cost(net) after multiplication(10 000): "+product_cost_result);
     }
 }

614f0c4f5594c4034076a769f3ab7a4336c118a8
Pavlo_Og <pavel.ognynanyk@gmail.com>
2023-10-18 22:28:33 +0200

    Updated BigDecimal rounding methods

--- src/task5.java
+++ src/task5.java
@@ -1,5 +1,5 @@
 import java.math.BigDecimal;
-import java.math.MathContext;
+import java.math.RoundingMode;
 
 import static java.math.BigDecimal.valueOf;
 
@@ -19,18 +19,19 @@ public class task5 {
 
         System.out.println("----------------------------------------------------");
         System.out.println("BigDecimal type:");
-        MathContext m1 = new MathContext(2);
         BigDecimal product_cost = new BigDecimal(9.99);
-        System.out.println("Product cost(net): "+product_cost.round(m1));
+        BigDecimal product_cost_rounded = product_cost.setScale(0, RoundingMode.FLOOR);
+        System.out.println("Product cost(net): "+product_cost_rounded);
         BigDecimal product_cost_gross = product_cost.multiply(valueOf(1.23));
-        System.out.println("Product cost(gross): "+product_cost_gross.round(m1));
-        MathContext m2= new MathContext(6);
+        BigDecimal product_cost_gross_rounded = product_cost_gross.setScale(0, RoundingMode.FLOOR);
+        System.out.println("Product cost(gross): "+product_cost_gross_rounded);
         BigDecimal product_cost_gross_mul = product_cost_gross.multiply(valueOf(10_000));
-        System.out.println("Product cost(gross) after multiplication(10 000): "+product_cost_gross_mul.round(m2));
+        BigDecimal product_cost_gross_mul_rounded = product_cost_gross_mul.setScale(0, RoundingMode.FLOOR);
+        System.out.println("Product cost(gross) after multiplication(10 000): "+product_cost_gross_mul_rounded);
         BigDecimal product_cost_result = product_cost_gross_mul.divide(valueOf(1.23));
-        MathContext m3= new MathContext(5);
-        System.out.println("Product cost(net) after multiplication(10 000): "+product_cost_result.round(m3));
-        // Last output: Product cost(net) after multiplication(10 000): 99900.0000000000021316282072803005576133728027343750000
+        BigDecimal product_cost_result_rounded = product_cost_result.setScale(0, RoundingMode.FLOOR);
+        System.out.println("Product cost(net) after multiplication(10 000): "+product_cost_result_rounded);
+        // Last output: Product cost(net) after multiplication(10 000): 99900
         System.out.println("----------------------------------------------------\n");
 
         System.out.println("----------------------------------------------------");
